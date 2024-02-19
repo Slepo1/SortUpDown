3ce7fc4eaed44936375900e03a7de6854efd2448
Slepo1 <lodka127@gmail.com>
2024-02-19 04:29:36 +0300

    OptimizeV1.1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
-// Тупая сортировка по возрастанию
+// Bubble sort по возрастанию
 #include <iostream> 
 
 using namespace std;
 
-int main()
+
+void sort(int numbers[], int n)
 {
-	int numbers[] {6, 0, 9, 3, 5, 1, 8, 2, 7, 4};
-
 	int tmp;
-
-	for(int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++) // sizeof(numbers)/sizeof(numbers[i]) можно узнать размер массива int'ов
+	for(int i = 0; i < n - 1; i++)
 	{
-		cout << numbers[i];
-	}
-
-	for(int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++)
-	{
-		for(int j = 0; j < sizeof(numbers)/sizeof(numbers[0]); j++)
+		for(int j = i + 1; j < n; j++)
 		{
-			if(numbers[i] < numbers[j])
+			if(numbers[i] > numbers[j])
 			{
 				tmp = numbers[i];
 				numbers[i] = numbers[j];
@@ -26,13 +19,27 @@ int main()
 			}
 		}
 	}
+}
 
-	cout << '\n';
-
-	for(int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++)
+void printNum(int numbers[], int n)
+{
+	for(int i = 0; i < n; i++)
 	{
-		cout << numbers[i];
+		cout << ' ' << numbers[i];
 	}
+	cout << '\n';
+}
+
+int main()
+{
+	int numbers[] {6, 0, 9, 3, 5, 1, 8, 2, 7, 4};
+	int n = sizeof(numbers)/sizeof(numbers[0]); // Размер массива
+
+	printNum(numbers, n);
+
+	sort(numbers, n);
+
+	printNum(numbers, n);
 
 	return 0;
 }
